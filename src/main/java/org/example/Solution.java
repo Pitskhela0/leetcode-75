766d3028f50a93052c3dae153916447b65a7f19c
Gakha Pitskhelauri <120589386+Pitskhela0@users.noreply.github.com>
2025-07-17 13:53:01 +0400

    solved 1657

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -484,9 +484,44 @@ public class Solution {
         return result;
     }
 
+    public boolean closeStrings(String word1, String word2) {
+        if(word1.length() != word2.length())
+            return false;
+
+        int[] w1 = new int[26];
+        int[] w2 = new int[26];
+
+        for (int i = 0; i < word1.length(); i++) {
+            w1[word1.charAt(i)-'a'] += 1;
+            w2[word2.charAt(i)-'a'] += 1;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if(w1[i] == w2[i]){
+                w1[i] = 0;
+                w2[i] = 0;
+            }
+        }
+        // caabbb
+        // abbccc
+        Arrays.sort(w1);
+        Arrays.sort(w2);
+
+        if(!Arrays.equals(w1, w2))
+            return false;
+
+        int nonZero = 0;
+        for (int i = 0; i < 26; i++) {
+            if(w1[i] != 0)
+                nonZero++;
+        }
+        return nonZero % 2 == 0;
+    }
+
+
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.maxOperations(new int[]{3,1,3,4,3}, 6));
+
     }
 }
